9d80296b5e0b412e4b364e3512970f2ff20a4346
Vlad Aparin <stinex.sx@gmail.com>
2023-03-13 09:19:31 +0500

    fix

--- app/data/job/job.data.tsx
+++ app/data/job/job.data.tsx
@@ -11,15 +11,15 @@ export const job: IJob[] = [
   {
     data_start: 1554076800,
     data_end: null,
-    job_name: 'Ergonova',
-    job_title: 'SIA Berry White',
+    job_name: 'SIA Berry White',
+    job_title: 'Frontend Developer',
     desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis deleniti amet quaerat dolorem iste, impedit omnis fuga quidem quas repellendus aliquam ipsa accusamus, obcaecati ipsum minus consequatur sequi nam culpa.',
   },
   {
     data_start: 1614556800,
     data_end: 1633046400,
-    job_name: 'Ergonova',
-    job_title: 'INDEX',
+    job_name: 'INDEX',
+    job_title: 'Frontend Developer',
     desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis deleniti amet quaerat dolorem iste, impedit omnis fuga quidem quas repellendus aliquam ipsa accusamus, obcaecati ipsum minus consequatur sequi nam culpa.',
   },
 ]
